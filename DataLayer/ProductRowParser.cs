8fa3e1f33da09f0c4d793eaf5723907928e5eb73
luigisenzamici <luigisenzamici@gmail.com>
2020-04-15 17:47:01 +0200

    fine per product

--- DataLayer/ProductRowParser.cs
+++ DataLayer/ProductRowParser.cs
@@ -10,9 +10,36 @@ namespace DataLayer
 {
     public class ProductRowParser : IProductRowParser
     {
+        private char separator;
+        public ProductRowParser(char separator)
+        {
+            this.separator = separator;
+        }
         public Product Parse(string row)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(row) || string.IsNullOrWhiteSpace(row)) throw new ArgumentNullException("row");
+            string[] splitted = row.Split(separator);
+            if (splitted.Length != 6) throw new FormatException("invalid format: row");
+            int Id = -1;
+            decimal Prezzo = 0M;
+            bool Attivo = false;
+
+            if (!int.TryParse(splitted[0], out Id)) throw new FormatException("invalid format: Id");
+            if (string.IsNullOrEmpty(splitted[1]) || string.IsNullOrWhiteSpace(splitted[1])) throw new FormatException("invalid format: Nome");
+            if (string.IsNullOrEmpty(splitted[2]) || string.IsNullOrWhiteSpace(splitted[2])) throw new FormatException("invalid format: Descrizione");
+            if (string.IsNullOrEmpty(splitted[3]) || string.IsNullOrWhiteSpace(splitted[3])) throw new FormatException("invalid format: Immagine");
+            if (!decimal.TryParse(splitted[4], out Prezzo)) throw new FormatException("invalid format: Prezzo");
+            if (!bool.TryParse(splitted[5], out Attivo)) throw new FormatException("invalid format: Attivo");
+
+            return new Product()
+            {
+                Id = Id,
+                Nome = splitted[1],
+                Descrizione = splitted[2],
+                Immagine = splitted[3],
+                Prezzo = Prezzo,
+                Attivo = Attivo
+            };
         }
     }
 }
--- DataLayer/ProductSerializer.cs
+++ DataLayer/ProductSerializer.cs
@@ -10,9 +10,17 @@ namespace DataLayer
 {
     public class ProductSerializer : IProductSerializer
     {
+        private char separator;
+        public ProductSerializer(char separator)
+        {
+            this.separator = separator;
+        }
         public string Serialize(Product product)
         {
-            throw new NotImplementedException();
+            if (product == null) throw new ArgumentNullException("objInstance");
+            var oi = product;
+            var s = separator;
+            return $"{oi.Id}{s}{oi.Nome}{s}{oi.Descrizione}{s}{oi.Immagine}{s}{oi.Prezzo}{s}{oi.Attivo}";
         }
     }
 }
