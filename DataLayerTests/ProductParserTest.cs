33430130274b58a7c2f46e3c6db8952326e4d51b
luigisenzamici <luigisenzamici@gmail.com>
2020-04-24 23:13:49 +0200

    aggiunto equalitycomparer corretto

--- DataLayerTests/ProductParserTest.cs
+++ DataLayerTests/ProductParserTest.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using DataLayer;
 using DomainModel;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DomainModel.EqualityComparer;
+using Xunit;
 
 namespace DataLayerTests
 {
-    [TestClass]
+    
     public class ProductParserTest
     {
         private Product productExpectedFactory(int Id) {
@@ -50,50 +51,50 @@ namespace DataLayerTests
             }
             return product;
         }
-        [TestMethod]
-        [DataRow("7|Mouse Pad mondo|Mouse pad con raffigurazione mappa mondiale|mousepadmondo_.jpg|12|False", 7 )]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True", 8)]
-        [DataRow("1|SmartWatch 2|Smartwatch con funzioni fitness, contapassi etc.|smartwatch-android-display.jpg|120,50|True", 1 )]
+        [Theory]
+        [InlineData("7|Mouse Pad mondo|Mouse pad con raffigurazione mappa mondiale|mousepadmondo_.jpg|12|False", 7 )]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True", 8)]
+        [InlineData("1|SmartWatch 2|Smartwatch con funzioni fitness, contapassi etc.|smartwatch-android-display.jpg|120,50|True", 1 )]
         public void ProductParserShouldPass_Test(string row, int Id)
         {
             var parser = new ProductRowParser('|');
             var expected = productExpectedFactory(Id);
             var actual = parser.Parse(row);
-            Assert.AreEqual(expected, actual, "Errore non sono identici");
+            Assert.Equal(expected, actual, new ProductEqualityComparer());
         }
-        [TestMethod]
-        [DataRow("a|Mouse Pad mondo|Mouse pad con raffigurazione mappa mondiale|mousepadmondo_.jpg|12|False")]
-        [DataRow("|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True")]
-        [DataRow("-1|SmartWatch 2|Smartwatch con funzioni fitness, contapassi etc.|smartwatch-android-display.jpg|120,50|True")]
-        [DataRow("8||Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True")]
-        [DataRow("8| |Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True")]
-        [DataRow("8|Mini Cuffie Stereo||cuffiefilo.jpg|16,00|True")]
-        [DataRow("8|Mini Cuffie Stereo| |cuffiefilo.jpg|16,00|True")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica||16,00|True")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|  |16,00|True")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg||True")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|a|True")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|-1|True")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00| ")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|a")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|55")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|")]
-        [DataRow("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|55|ab")]
+        [Theory]
+        [InlineData("a|Mouse Pad mondo|Mouse pad con raffigurazione mappa mondiale|mousepadmondo_.jpg|12|False")]
+        [InlineData("|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True")]
+        [InlineData("-1|SmartWatch 2|Smartwatch con funzioni fitness, contapassi etc.|smartwatch-android-display.jpg|120,50|True")]
+        [InlineData("8||Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True")]
+        [InlineData("8| |Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|True")]
+        [InlineData("8|Mini Cuffie Stereo||cuffiefilo.jpg|16,00|True")]
+        [InlineData("8|Mini Cuffie Stereo| |cuffiefilo.jpg|16,00|True")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica||16,00|True")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|  |16,00|True")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg||True")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|a|True")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|-1|True")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00| ")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|a")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|55")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|")]
+        [InlineData("8|Mini Cuffie Stereo|Mini cuffie stereo con microfono per chiamate e ascoltare musica|cuffiefilo.jpg|16,00|55|ab")]
         public void ProductParser_ShouldRaiseAFormatException_Tests(string row)
         {
             var parser = new ProductRowParser('|');
-            Assert.ThrowsException<FormatException>(() => parser.Parse(row));
+            Assert.Throws<FormatException>(() => parser.Parse(row));
         }
 
-        [TestMethod]
-        [DataRow("")]
-        [DataRow("  ")]
-        [DataRow(null)]
+        [Theory]
+        [InlineData("")]
+        [InlineData("  ")]
+        [InlineData(null)]
         public void ProductParser_ShouldRaiseArgumentNullException_Test(string row)
         {
             var parser = new ProductRowParser('|');
-            Assert.ThrowsException<ArgumentNullException>(() => parser.Parse(row));
+            Assert.Throws<ArgumentNullException>(() => parser.Parse(row));
         }
 
     }
---
+++ DomainModel/EqualityComparer/ProductEqualityComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.EqualityComparer
+{
+    public class ProductEqualityComparer : IEqualityComparer<Product>
+    {
+        public bool Equals(Product x, Product y)
+        {
+            return x.Id == y.Id &&
+                    x.Nome == y.Nome &&
+                    x.Descrizione == y.Descrizione &&
+                    x.Immagine == y.Immagine &&
+                    x.Prezzo == y.Prezzo &&
+                    x.Attivo == y.Attivo;
+        }
+
+        public int GetHashCode(Product obj)
+        {
+            int hash = 13;
+            hash = (hash * 7) + obj.Id.GetHashCode();
+            hash = (hash * 7) + obj.Nome.GetHashCode();
+            hash = (hash * 7) + obj.Descrizione.GetHashCode();
+            hash = (hash * 7) + obj.Immagine.GetHashCode();
+            hash = (hash * 7) + obj.Prezzo.GetHashCode();
+            hash = (hash * 7) + obj.Attivo.GetHashCode();
+            return hash;
+        }
+    }
+}
